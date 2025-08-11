1836acf78c3bfdb0259450d969b633fc039329d8
amirhossein-unlimit <amirhossein.unlimited@gmail.com>
2025-08-12 03:15:50 +0330

    feat(docs): add initial docs with 3 pages – start, basics & everyday types

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  extends: ["shadcn-docs-nuxt"],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "fa",
+      },
+    },
+  },
+  i18n: {
+    defaultLocale: "fa",
+    locales: [{ code: "fa", name: "فارسی", language: "fa-IR" }],
+  },
+  compatibilityDate: "2024-07-06",
+});
