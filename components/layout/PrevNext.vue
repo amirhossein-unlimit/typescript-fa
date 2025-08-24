018b25e127583a7316607f637e5f9e531b1bcd88
amirhossein-unlimit <amirhossein.unlimited@gmail.com>
2025-08-24 03:35:25 +0330

    style: add spacing between previous and next post buttons

--- components/layout/PrevNext.vue
+++ components/layout/PrevNext.vue
@@ -1,7 +1,5 @@
 <template>
-  <!-- lg:flex lg:flex-row lg:gap-4 lg:items-stretch -->
-  <!-- class="border-t pt-6 pb-4 grid grid-cols-1 lg:grid-cols-2 gap-4" -->
-  <div v-if="page.prevNext ?? true" class="border-t pt-6 pb-4 lg:flex lg:flex-row lg:items-stretch gap-4" :class="{ 'justify-end': !!next }">
+  <div v-if="page.prevNext ?? true" class="border-t pt-6 pb-4 flex flex-col lg:flex-row lg:items-stretch gap-4" :class="{ 'justify-end': !!next }">
     <LayoutPrevNextButton :prev-next="prev" side="left" />
     <LayoutPrevNextButton :prev-next="next" side="right" />
   </div>
